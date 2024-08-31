be2147ed988c34fe032ad83e5a9015683d0f9ce3
aldenling <aldenling17@gmail.com>
2024-08-31 12:53:37 +0800

    added the main page
    may select which subsystem want to test

--- charity centre management system/src/CharityCentreManagementSystem/mainfile.java
+++ charity centre management system/src/CharityCentreManagementSystem/mainfile.java
@@ -2,11 +2,23 @@ package CharityCentreManagementSystem;
 
 import static Managment.DDSubsystem.DonationDistributionMainPage;
 import static Managment.DonationManagement.DonationManagement;
+import java.util.Scanner;
 
 public class mainfile {
-    public static void main(String[] args)
-    {
-        DonationManagement();
-        DonationDistributionMainPage();
+
+    public static void main(String[] args) {
+        System.out.print("Select your Subsystem:");
+        System.out.print("-------------------------------");
+        System.out.print("1. Donation Managment ");
+        System.out.print("2. Donation Distribution");
+        System.out.print("-------------------------------");
+        System.out.print("Selection:");
+        int anw = new Scanner(System.in).nextInt();
+
+        if (anw == 1) {
+            DonationManagement();
+        } else if (anw == 2) {
+            DonationDistributionMainPage();
+        }
     }
 }
