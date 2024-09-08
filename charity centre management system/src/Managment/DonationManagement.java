11773297984e182a0e58c68cde5332cc0e0e56f0
VeyronHeng <126947918+VeyronHeng@users.noreply.github.com>
2024-09-08 15:08:44 +0800

    5

--- charity centre management system/src/Managment/DonationManagement.java
+++ charity centre management system/src/Managment/DonationManagement.java
@@ -836,13 +836,5 @@ public class DonationManagement{
             e.printStackTrace();
         }
     }
-
-    private static void EventsLists() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    private static void EventList() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
 }
 
